727f09b2f73757a2ebd0d2d7462ac0719accdac9
Klarys <44509197+Klarys@users.noreply.github.com>
2020-11-16 19:34:41 +0100

    added left and right move buttons

--- adaline-ex2.py
+++ adaline-ex2.py
@@ -4,7 +4,7 @@ import random
 
 pygame.init()
 pygame.font.init()
-myfont = pygame.font.SysFont('Open Sans', 15)
+myfont = pygame.font.SysFont('Open Sans', 24)
 screen = pygame.display.set_mode([550, 600])
 values = np.zeros((7,7))
 rects = []
@@ -20,12 +20,12 @@ number[0] = [
     [0.0,0.0,0.0,0.0,0.0,0.0,0.0]
 ]
 number[1] = [
-    [0.0,0.0,0.0,0.0,0.0,.0,0.0],
-    [0.0,0.0,0.0,0.0,1.0,0.0,0.0],
-    [0.0,0.0,0.0,1.0,1.0,0.0,0.0],
-    [0.0,0.0,1.0,0.0,1.0,0.0,0.0],
-    [0.0,0.0,0.0,0.0,1.0,0.0,0.0],
-    [0.0,0.0,0.0,0.0,1.0,0.0,0.0],
+    [0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+    [0.0,0.0,0.0,1.0,0.0,0.0,0.0],
+    [0.0,0.0,1.0,1.0,0.0,0.0,0.0],
+    [0.0,1.0,0.0,1.0,0.0,0.0,0.0],
+    [0.0,0.0,0.0,1.0,0.0,0.0,0.0],
+    [0.0,0.0,0.0,1.0,0.0,0.0,0.0],
     [0.0,0.0,0.0,0.0,0.0,0.0,0.0]
 ]
 number[2] = [
@@ -103,10 +103,9 @@ number[9] = [
 training_inputs = []
 training_inputs = [ np.ravel(n) for n in number ]
 allLabels = []
-no_of_ui_buttons = 15
+no_of_ui_buttons = 16
 buttonHeight = 20
 startingWeights = []
-currentTrainingType = -1 #-1 for none, 0 - basic, 1-SPLA, 2-PLA, 3-RPLA
 buttonsPressed = []
 
 class Perceptron(object):
@@ -130,93 +129,6 @@ class Perceptron(object):
         prediction = self.output(input)
         self.weights[1:] += self.learning_rate * (label - prediction) * input
         self.weights[0] += self.learning_rate * (label - prediction)
-    global currentTrainingType
-    currentTrainingType = 0
-
-  def trainSPLA(self, training_data, labels):
-        allCorrect = False
-        zippedList = list(zip(training_data, labels))
-
-        while allCorrect == False:
-            allCorrect = True
-            randomIndex = random.randint(0, len(zippedList) - 1)
-
-            input = zippedList[randomIndex][0]
-            label = zippedList[randomIndex][1]
-            input = self.noisy(np.copy(input))
-            prediction = self.output(input)
-            err = label - prediction
-            if err != 0:
-                self.weights[1:] += self.learning_rate * err * input
-                self.weights[0] += self.learning_rate * err
-                allCorrect = False
-            else:
-                if self.checkAllPredictions(zippedList) == False:
-                    allCorrect = False
-        global currentTrainingType
-        currentTrainingType = 1
-
-  def trainPLA(self, training_data, labels):
-        allCorrect = False
-        zippedList = list(zip(training_data, labels))
-        bestWeights = np.zeros(self.no_of_inputs + 1)
-        bestWeightsLife = 0
-        weightsLife = 0
-
-        for _ in range(self.iterations):
-            randomIndex = random.randint(0, len(zippedList) - 1)
-
-            input = zippedList[randomIndex][0]
-            label = zippedList[randomIndex][1]
-            input = self.noisy(np.copy(input))
-            prediction = self.output(input)
-            err = label - prediction
-            if err != 0:
-                self.weights[1:] += self.learning_rate * err * input
-                self.weights[0] += self.learning_rate * err
-                weightsLife = 0
-            else:
-                weightsLife += 1
-                if weightsLife > bestWeightsLife:
-                    bestWeightsLife = weightsLife
-                    bestWeights = self.weights
-        
-        self.weights = bestWeights
-        global currentTrainingType
-        currentTrainingType = 2
-
-  def trainRPLA(self, training_data, labels):
-        allCorrect = False
-        zippedList = list(zip(training_data, labels))
-        bestWeights = np.zeros(self.no_of_inputs + 1)
-        bestWeightsLife = 0
-        bestWeightsCorrectExamples = 0
-        weightsLife = 0
-        correctExamples = 0
-        for _ in range(self.iterations):
-            randomIndex = random.randint(0, len(zippedList) - 1)
-
-            input = zippedList[randomIndex][0]
-            label = zippedList[randomIndex][1]
-            input = self.noisy(np.copy(input))
-            prediction = self.output(input)
-            err = label - prediction
-            if err != 0:
-                self.weights[1:] += self.learning_rate * err * input
-                self.weights[0] += self.learning_rate * err
-                weightsLife = 0
-                correctExamples = 0
-            else:
-                weightsLife += 1
-                correctExamples = self.correctPredictions(zippedList)
-                if weightsLife > bestWeightsLife and correctExamples > bestWeightsCorrectExamples:
-                    bestWeightsLife = weightsLife
-                    bestWeights = self.weights
-                    bestWeightsCorrectExamples = correctExamples
-        
-        self.weights = bestWeights
-        global currentTrainingType
-        currentTrainingType = 3
 
   def correctPredictions(self, training_data_list):
         result = 0
@@ -293,7 +205,7 @@ def drawMessage(message):
     pygame.display.flip()
 
 def drawUI():
-    buttonTexts = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Reset", "train PLA", "train SPLA", "train RPLA", "Generate noise"]
+    buttonTexts = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Reset", "<-", "->", "^", "v", "Generate noise"]
     
     for buttonNumber in range(len(buttonTexts)):
         pygame.draw.rect(screen, (102, 153, 255), pygame.Rect(300,10 + (buttonHeight+15)*buttonNumber,170,buttonHeight))
@@ -334,56 +246,18 @@ def addExample(buttonNumber):
     for i in range(10):
         perceptrons[i].weights = np.random.rand(perceptrons[i].no_of_inputs + 1)
         perceptrons[i].weights = perceptrons[i].weights/10
-
-    if(currentTrainingType == 0):
-        for i in range(10):
-            labelsCopy = allLabels[i].copy()
-            print("Range:")
-            print(range(len(buttonsPressed)))
-            for j in range(len(buttonsPressed)):
-                if i == buttonsPressed[j]:
-                    newLabel = 1.0
-                else:
-                    newLabel = 0.0
-                labelsCopy = np.append(labelsCopy, newLabel)
-            perceptrons[i].train(training_inputs, labelsCopy)
-    elif(currentTrainingType == 1):
-        for i in range(10):
-            labelsCopy = allLabels[i].copy()
-            print("Range:")
-            print(range(len(buttonsPressed)))
-            for j in range(len(buttonsPressed)):
-                if i == buttonsPressed[j]:
-                    newLabel = 1.0
-                else:
-                    newLabel = 0.0
-                labelsCopy = np.append(labelsCopy, newLabel)
-            perceptrons[i].trainSPLA(training_inputs, labelsCopy)
-    elif(currentTrainingType == 2):
-        for i in range(10):
-            labelsCopy = allLabels[i].copy()
-            print("Range:")
-            print(range(len(buttonsPressed)))
-            for j in range(len(buttonsPressed)):
-                if i == buttonsPressed[j]:
-                    newLabel = 1.0
-                else:
-                    newLabel = 0.0
-                labelsCopy = np.append(labelsCopy, newLabel)
-            perceptrons[i].trainPLA(training_inputs, labelsCopy)
-    elif(currentTrainingType == 3):
-        for i in range(10):
-            labelsCopy = allLabels[i].copy()
-            print("Range:")
-            print(range(len(buttonsPressed)))
-            for j in range(len(buttonsPressed)):
-                if i == buttonsPressed[j]:
-                    newLabel = 1.0
-                else:
-                    newLabel = 0.0
-                labelsCopy = np.append(labelsCopy, newLabel)
-            perceptrons[i].trainRPLA(training_inputs, labelsCopy)
-
+    
+    for i in range(10):
+        labelsCopy = allLabels[i].copy()
+        print("Range:")
+        print(range(len(buttonsPressed)))
+        for j in range(len(buttonsPressed)):
+            if i == buttonsPressed[j]:
+                newLabel = 1.0
+            else:
+                newLabel = 0.0
+            labelsCopy = np.append(labelsCopy, newLabel)
+        perceptrons[i].train(training_inputs, labelsCopy)
 
 def buttonZero():
     print("button Zero clicked")
@@ -404,12 +278,12 @@ def buttonOne():
     global values
     buttonsPressed.append(1)
     values = np.array([
-    [0.0,0.0,0.0,0.0,0.0,.0,0.0],
-    [0.0,0.0,0.0,0.0,0.0,1.0,0.0],
-    [0.0,0.0,0.0,0.0,1.0,1.0,0.0],
-    [0.0,0.0,0.0,1.0,0.0,1.0,0.0],
-    [0.0,0.0,0.0,0.0,0.0,1.0,0.0],
-    [0.0,0.0,0.0,0.0,0.0,1.0,0.0],
+    [0.0,0.0,0.0,0.0,0.0,0.0,0.0],
+    [0.0,0.0,0.0,1.0,0.0,0.0,0.0],
+    [0.0,0.0,1.0,1.0,0.0,0.0,0.0],
+    [0.0,1.0,0.0,1.0,0.0,0.0,0.0],
+    [0.0,0.0,0.0,1.0,0.0,0.0,0.0],
+    [0.0,0.0,0.0,1.0,0.0,0.0,0.0],
     [0.0,0.0,0.0,0.0,0.0,0.0,0.0]
     ])
 
@@ -540,50 +414,41 @@ def buttonReset():
         labels[i] = 1
         perceptrons[i].train(training_inputs, labels)
 
-def buttonPLA():
-    print("button PLA clicked")
-    global values
-    values = np.zeros((7,7))
-    global buttonsPressed
-    buttonsPressed = []
-    for i in range(10):
-        perceptrons[i].weights = np.random.rand(perceptrons[i].no_of_inputs + 1)
-        perceptrons[i].weights = perceptrons[i].weights/10
+def buttonLeft():
+    print("button Left clicked")
+    rows, cols = values.shape
+    print(cols)
+    for row in range(rows):
+        for col in range(1, cols):
+            print(f'Row: {row}, col: {col}')
+            if values[row][col] == 1:
+                values[row][col-1] = 1.0
+                values[row][col] == 0.0
+            else:
+                values[row][col-1] = 0.0
+        values[row][cols-1] = 0.0
 
-    for i in range(10):
-        labels = np.zeros(10)
-        labels[i] = 1
-        perceptrons[i].trainPLA(training_inputs, labels)
+def buttonRight():
+    print("button Right clicked")
+    print("button Left clicked")
+    rows, cols = values.shape
+    print(cols)
+    for row in range(rows):
+        for col in reversed(range(cols-1)):
+            print(f'Row: {row}, col: {col}')
+            if values[row][col] == 1:
+                values[row][col+1] = 1.0
+                values[row][col] == 0.0
+            else:
+                values[row][col+1] = 0.0
+        values[row][0] = 0.0
 
-def buttonSPLA():
-    print("button SPLA clicked")
-    global values
-    values = np.zeros((7,7))
-    global buttonsPressed
-    buttonsPressed = []
-    for i in range(10):
-        perceptrons[i].weights = np.random.rand(perceptrons[i].no_of_inputs + 1)
-        perceptrons[i].weights = perceptrons[i].weights/10
+def buttonUp():
+    print("button Up clicked")
 
-    for i in range(10):
-        labels = np.zeros(10)
-        labels[i] = 1
-        perceptrons[i].trainSPLA(training_inputs, labels)
+def buttonDown():
+    print("button Down clicked")
 
-def buttonRPLA():
-    print("button RPLA clicked")
-    global values
-    values = np.zeros((7,7))
-    global buttonsPressed
-    buttonsPressed = []
-    for i in range(10):
-        perceptrons[i].weights = np.random.rand(perceptrons[i].no_of_inputs + 1)
-        perceptrons[i].weights = perceptrons[i].weights/10
-
-    for i in range(10):
-        labels = np.zeros(10)
-        labels[i] = 1
-        perceptrons[i].trainRPLA(training_inputs, labels)
 
 def buttonNoise():
     print("button Noise clicked")
@@ -607,10 +472,11 @@ def activateButton(buttonNumber):
         8: buttonEight,
         9: buttonNine,
         10: buttonReset,
-        11: buttonPLA,
-        12: buttonSPLA,
-        13: buttonRPLA,
-        14: buttonNoise
+        11: buttonLeft,
+        12: buttonRight,
+        13: buttonUp,
+        14: buttonDown,
+        15: buttonNoise
     }
     # Get the function from switcher dictionary
     func = switcher.get(buttonNumber, lambda: "Invalid button number")
@@ -686,8 +552,6 @@ def main():
                     drawPerceptronsOutput()
                 elif detectedButton == 10:
                     drawMessage("Reset finished.")
-                else:
-                    drawMessage("Training finished.")
                 draw_rectangles()
                 drawUI()
                 
